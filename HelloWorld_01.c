7228b0dcf1d546d7ee766ea236b367fe6c133865
João Victor de Castro Ferro <88172545+TotalRandon@users.noreply.github.com>
2022-02-17 16:07:13 -0300

    Add files via upload

---
+++ HelloWorld_01.c
@@ -0,0 +1,10 @@
+#include <stdio.h>// Biblioteca basica de inicio
+
+int main()// função principal
+{
+    printf("Hello,World!\n");// O computador mostrará na tela do cmd o que estiver no parenteses
+    return 0;// valor deve ser retornavel por ser função "int"
+}
+
+// = comentario em uma unica linha
+/* = comentario de tamanho determinado */ 
